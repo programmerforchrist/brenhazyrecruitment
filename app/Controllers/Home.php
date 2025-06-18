9aff4c65a19ceeb5f06bf6fe7e97e591b039c2c0
Chinedum Tobias Nnaji <213635443+programmerforchrist@users.noreply.github.com>
2025-06-18 12:45:14 +0100

    Add homepage view and edit the master view/page
    Added home.php in the View folder and edited the
    master view(app.php) to include links to
    dynamically link to the login view and register
    view that is yet to be created/established.

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,6 +7,6 @@ class Home extends BaseController
     public function index(): string
     {
        // return view('welcome_message');
-       return "Jesus is Lord!!! Learning to Build Web Apps using Code Igniter";
+       return view('home');
     }
 }
